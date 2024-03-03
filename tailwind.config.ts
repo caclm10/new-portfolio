fad63b93b47f26d1df991d662e2b10fdd42746f5
caclm10 <lewinxander@gmail.com>
2024-03-03 16:06:29 +0700

    fix: preline path in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/preline/dist/*.js',
+    './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {
