c64c5a777886cfc4c0b80255c241b2ac8dcb2150
caclm10 <lewinxander@gmail.com>
2025-03-20 11:25:31 +0700

    feat: add base layout and font setup

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+const metadata: Metadata = {
+    title: {
+        template: "%s - Lewin Xander Gulo",
+        absolute: "Lewin Xander Gulo",
+    },
+    description:
+        "Welcome to my personal portfolio. My Name is Lewin Xander and I am a web developer with expertise in web development, mainly using the Laravel framework. Explore my work, discover my latest projects, and learn more about my web development expertise.",
+};
+
+function MainLayout({ children }: React.PropsWithChildren) {
+    return (
+        <>
+            {/*  */}
+
+            {children}
+        </>
+    );
+}
+
+export { metadata };
+
+export default MainLayout;
---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+function IndexPage() {
+    return (
+        <div>
+            {/*  */}
+            <p>Index Page</p>
+        </div>
+    );
+}
+
+export default IndexPage;
